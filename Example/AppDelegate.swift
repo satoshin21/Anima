fb1442f68a0bd956b98b66092e98cdc549bde21f
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2017-10-03 13:52:39 +0900

    LargeTitleDisplay for iOS11

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -20,6 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         navAppearance.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         navAppearance.barTintColor = .navigationBar
         navAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        if #available(iOS 11.0, *) {
+            navAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        }
         UITableView.appearance().backgroundColor = .background
         
         UIButton.appearance().tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
