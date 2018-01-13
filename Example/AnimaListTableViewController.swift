59622e2498674e21a60b15ace1456f3f4690f77d
satoshin21 <satoshi.nagasaka21@gmail.com>
2018-01-13 23:34:15 +0900

    fixed example background

--- Example/AnimaListTableViewController.swift
+++ Example/AnimaListTableViewController.swift
@@ -30,11 +30,7 @@ class AnimaListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        view.backgroundColor = .background
     }
 
     override func didReceiveMemoryWarning() {
